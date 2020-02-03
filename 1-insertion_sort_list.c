ef2db3845e807935d3d6a1d62544f56059271003
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-03 18:07:29 +0000

    Add validations 1-insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -61,7 +61,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *c = *list;
 
-	if (c == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (c->next != NULL)
