72dd0253bce368e8b7a420c85a9a98a06b2d83c6
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-03 21:52:53 +0000

    Change implementation of 2-selection_sort 2.0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,20 +20,23 @@ void _swap(int *a, int *b)
 
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	unsigned int i, j, min;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-			{
-				_swap(&array[i], &array[j]);
-				print_array(array, size);
-			}
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			_swap(&array[i], &array[min]);
+			print_array(array, size);
 		}
 	}
 
