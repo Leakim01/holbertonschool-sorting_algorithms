3f0fac88503502f73dd7844e09ae1331fc1f4830
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-03 21:06:09 +0000

    Remove cp copy variable

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -21,24 +21,18 @@ void _swap(int *array, int a, int b)
 
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, cp;
+	unsigned int i, j;
 
 	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		cp = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[cp] > array[j])
+			if (array[i] > array[j])
 			{
-				_swap(array, cp, j);
-				if (array[cp] < array[i])
-				{
-					_swap(array, cp, i);
-				}
-				cp = j;
+				_swap(array, i, j);
 				print_array(array, size);
 			}
 		}
