67d769d46c74866243477bc185be59cbd3cde2e5
jorgechauxjr <chauxjorge@hotmail.com>
2020-02-05 23:41:42 +0000

    Remove validation array null

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -40,7 +40,7 @@ void shell_sort(int *array, size_t size)
 {
 	unsigned int flag = 0, gap = 1, i, j;
 
-	if (size < 2 || array == NULL)
+	if (size < 2)
 		return;
 	while (gap < size / 3)
 		gap = gap * 3 + 1;
